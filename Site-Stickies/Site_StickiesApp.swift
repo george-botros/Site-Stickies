79111e37133dd6ad1cdab885bcbf20e56f1af6a4
George Botros <georgebotros02@gmail.com>
2021-02-10 02:57:13 -0500

    Initial Commit

---
+++ Site-Stickies/Site_StickiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Site_StickiesApp.swift
+//  Site-Stickies
+//
+//  Created by George Botros on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Site_StickiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
